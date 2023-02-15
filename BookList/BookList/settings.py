9950dd886a28e3e97b7c66d28935206724f65a69
KIM <2016124068@kau.kr>
2023-02-15 17:02:55 +0900

    #2 DRF API class_based_view with methods Like PUT, POST, GET

--- BookList/BookList/settings.py
+++ BookList/BookList/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "BookListAPI",
+    "debug_toolbar",
     
-
 ]
 
 MIDDLEWARE = [
--- BookList/BookListAPI/urls.py
+++ BookList/BookListAPI/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.books),
-    
+    #path('books/', views.books),
+    # 2
+    path('books/', views.BookList.as_view()),
+    path('books/<int:pk>', views.Book.as_view()),
+
 ]
