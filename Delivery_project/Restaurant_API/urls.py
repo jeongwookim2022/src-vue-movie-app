d9ded4fa4b685df1000311c8f259d54c06fafd36
KIM <2016124068@kau.kr>
2023-02-23 14:22:15 +0900

    Updated urls.py of APP and PROEJCT

--- Delivery_project/Restaurant_API/urls.py
+++ Delivery_project/Restaurant_API/urls.py
@@ -4,7 +4,13 @@ from . import views
 urlpatterns = [
     path('category/', views.CategoriesView.as_view()),
     path('menu-items/', views.MenuitemsView.as_view()),
-    path('groups/manager/users/', views.managers),
-
+    path('menu-items/<int:pk>/',views.SingleMenuItemView.as_view()),
+    path('cart/menu-items/', views.CartView.as_view()),
+    path('orders/', views.OrderView.as_view()),
+    path('orders/<int:pk>/', views.OrderView.as_view()),
+    path('groups/manager/users/', views.GroupViewSet.as_view(
+        {"get":"list", 'post': 'create', 'delete':'destroy'})),
+    path('groups/delivery-crew/users/', views.DeliveryCrewViewSet.as_view(
+        {"get":"list", 'post': 'create', 'delete':'destroy'})),
     
 ]
